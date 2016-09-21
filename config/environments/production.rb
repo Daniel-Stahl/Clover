d1513e6389c7e52c00a1c588e82b603145cd1532
Daniel Stahl <danny.stahl@gmail.com>
2016-09-20 20:32:45 -0700

    config s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,12 @@ Rails.application.configure do
 
   #Sets Paperclip to uplod to Amazon S3 by default
   config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
+    :storage => :s3,
+    :s3_region => ENV['AWS_S3_REGION'],
+    :s3_credentials => {
+      :bucket => ENV['AWS_S3_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
 end
